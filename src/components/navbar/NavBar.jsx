8fd454e88af252135e3f91d4fc29dada7ca44abb
LiamEarl <70294109+LiamEarl@users.noreply.github.com>
2025-03-01 02:27:56 -0800

    Initial Commit

---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./NavBar.css"
+
+const NavBar = () => {
+    return (
+        <div className='navbar'>
+            <div className="nav-menu">
+                <l1>Home</l1>
+                <l1>Skills</l1>
+                <l1>Projects</l1>
+                <l1>Contact</l1>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
