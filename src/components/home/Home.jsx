aac39bb81453489bd4741a0f14fe1f4b6b3ce64e
LiamEarl <70294109+LiamEarl@users.noreply.github.com>
2025-03-03 18:14:19 -0800

    Home page is mostly done, preliminary skills section.

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+import profile_picture from '../../assets/profile-picture.jpg'
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <img/>
+        </div>
+    )
+}
+
+export default Home
